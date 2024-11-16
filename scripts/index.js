9eef6a6de8b340a506b972250583377a71cc3125
Oladipupo Nathan Akanji <dipoakanji57@gmail.com>
2024-11-16 21:24:06 +0100

    Organized structuring

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,21 @@
-import { boxToBeShown } from "./modules.js";
-import { HTMLObject } from "./modules.js";
-
 const resultArea = document.getElementById("footer");
 const renderButton = document.getElementById("run");
 
 renderButton.onclick = () => {
+  const HTMLObject = {
+    topic: "This is a custom box",
+    text: {
+      name: "Hello, I am Dipo",
+      age: "I am nine years old",
+    },
+  };
+  const boxToBeShown = `
+    <main>
+      <div class="container">
+        <h1 class="container-name">${HTMLObject.topic}</h1>
+        <p>${HTMLObject.text.name} <br/> ${HTMLObject.text.age}</p>
+      </div>
+    </main>
+`;
   resultArea.innerHTML = boxToBeShown;
 };
